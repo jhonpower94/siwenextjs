3a2eb07cd7ba8f79d789012c76f7fbdde7fc4cc7
jhon <jhonsnow751@gmail.com>
2025-03-05 21:52:22 +0100

    Vdhuvdhubvjd

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,38 @@
-import Image from "next/image";
+"use client";
+import {
+  getAddressFromMessage,
+  getChainIdFromMessage,
+  verifySignature,
+} from "@reown/appkit-siwe";
+import styles from "./page.module.css";
+import { projectId } from "./config/index";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const message = atob(
+    "bG9jYWxob3N0OjMwMDAgd2FudHMgeW91IHRvIHNpZ24gaW4gd2l0aCB5b3VyIEV0aGVyZXVtIGFjY291bnQ6CjB4MjVlOTk0ZGE3NDMyMzc0MDI0N2I0NjlDOTY2MzJiMDUzODI4YTREOAoKUGxlYXNlIHNpZ24gd2l0aCB5b3VyIGFjY291bnQKClVSSTogaHR0cDovL2xvY2FsaG9zdDozMDAwClZlcnNpb246IDEKQ2hhaW4gSUQ6IDEKTm9uY2U6IDMzMzY5NzZlZmE5MmMxOTc1NTA0YjAzNjdiNjE4NGEyZjFmNWE5NzIxYjViY2YxZWYzYzI3MzNkZTAzNzY2MDEKSXNzdWVkIEF0OiAyMDI1LTAzLTA1VDA2OjMyOjU0LjU1OFo="
+  );
+  const chainId = getChainIdFromMessage(message);
+  const address = getAddressFromMessage(message);
+  const signature =
+    "0x5d77f6622a0debfa1ad99562d838e322c7268d2d20fa0e475ce7423396ca2248759367d8b98d2fea2ca061c120ad54f57e9b75a6f273d0a8654b759a01fcf2f31b";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const handleVerMsg = async () => {
+    const isValid = await verifySignature({
+      address,
+      message,
+      signature,
+      chainId,
+      projectId,
+    });
+    // return isValid;
+    console.log(isValid);
+    console.log(address);
+  };
+
+  return (
+    <main className={styles.main}>
+      <appkit-button />
+      <button onClick={handleVerMsg}>Verify</button>
+    </main>
   );
 }
