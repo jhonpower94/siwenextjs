8b9214e3710952a51169a3d81b75c2015129a13d
jhon <jhonsnow751@gmail.com>
2025-07-14 08:32:20 +0100

    Vfdhcgudncvdfcedfhf

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-    webpack: (config) => {
-      config.externals.push("pino-pretty", "lokijs", "encoding");
-      return config;
-    }
-  };
-  
-  export default nextConfig;
-  
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+};
+
+export default nextConfig;
